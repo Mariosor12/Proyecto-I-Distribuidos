4ea339aa351e488e3a786e8969deb26c25a8142e
Tomás <tomasg92@gmail.com>
2021-12-02 09:12:31 -0400

    Cambio de IPs

--- calc_rmi/src/Server.java
+++ calc_rmi/src/Server.java
@@ -3,11 +3,11 @@ import java.rmi.Naming;
 public class Server {
   public static void main(String[] args) {
     System.out.println("Launching server...");
-    System.setProperty("java.rmi.server.hostname", "10.0.2.4");
+    System.setProperty("java.rmi.server.hostname", "10.0.2.5");
 
     try {
       Adder adder = new Adder();
-      String url = "rmi://10.0.2.4:1099/adder";
+      String url = "rmi://10.0.2.5:1099/adder";
       Naming.rebind(url, adder);
       System.out.println("Server listening...");
 
