a1e7949281f720c3b353abd9c66bcaf7fa8fa9cb
Tomás <tomasg92@gmail.com>
2021-12-02 11:09:20 -0400

    modifica oootra vez la ip

--- calc_rmi/src/Server.java
+++ calc_rmi/src/Server.java
@@ -3,11 +3,11 @@ import java.rmi.Naming;
 public class Server {
   public static void main(String[] args) {
     System.out.println("Launching server...");
-    System.setProperty("java.rmi.server.hostname", "192.168.0.106");
+    System.setProperty("java.rmi.server.hostname", "192.168.56.103");
 
     try {
       Adder adder = new Adder();
-      String url = "rmi://192.168.0.106/adder";
+      String url = "rmi://192.168.56.103/adder";
       Naming.rebind(url, adder);
       System.out.println("Server listening...");
 
