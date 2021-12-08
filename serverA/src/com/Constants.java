888ceeadf027e07938dc66c5d8d070ae256f9ea9
Tomás <tomasg92@gmail.com>
2021-12-07 23:02:35 -0400

    Elimina Constants.java

--- serverA/src/com/Constants.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public final class Constants {
-  private Constants() {
-  }
-
-  public static final String IP = "192.168.250.10";
-  public static final String URL = "rmi://" + IP + "/adder";
-}
