c668f34b6e93e25019315a540237fda96ff63385
Tomás <tomasg92@gmail.com>
2021-12-08 00:46:41 -0400

    Agrega verbosidad al build.sh

--- build.sh
+++ build.sh
@@ -24,6 +24,8 @@ cp Constants.java server$lib/src/com
 cp Constants.java client$lib/src/com
 rm -f Constants.java
 
+echo "Constants file created with IP $1"
+
 cd server$lib
 bash compile.sh
 echo "Server $lib compiled successfully"
