d67769470f46e41f8e3a0c56fab8cc5ec4e0de04
Tomás <tomasg92@gmail.com>
2021-12-09 18:14:32 -0400

    Recrrea interfaz de Main java

--- clientA/src/com/Main.java
+++ clientA/src/com/Main.java
@@ -2,6 +2,7 @@ package com;
 
 import java.rmi.Naming;
 import java.util.*;
+import java.util.regex.Pattern;
 
 import com.books.services.IBooksService;
 import com.books.views.BooksViews;
@@ -25,67 +26,104 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             UIService ui = new UIService();
             IBooksService booksService;
-            int option;
+            String command;
             Trace trace;
 
             ui.print("Choose a library (A, B, C or blank)");
             String library = scanner.nextLine();
 
+            if (library.isEmpty()) {
+                library = "A";
+            }
+
             switch (library) {
                 case "A":
                     booksService = remoteService(Constants.URL_A);
 
-                    ui.print("1. Get Book by name");
-                    ui.print("2. Get Books by author");
-                    option = scanner.nextInt();
+                    ui.print("Available commands for library " + library);
+                    ui.print("1. Pedir Libro [LIBRO]");
+                    ui.print("2. Pedir Autor [AUTOR]");
 
-                    switch (option) {
-                        case 1:
-                            ui.print("Book title?");
-                            // String bookTitle = scanner.nextLine();
-                            String bookTitle = "Dune";
+                    command = scanner.nextLine();
 
-                            booksViews.printBook(booksService.getBookByName(bookTitle));
-                            ui.saveTrace(library, "getBookByName", bookTitle);
-                            break;
-                        case 2:
-                            ui.print("Author name?");
-                            String authorName = scanner.nextLine();
+                    if (command.matches("Pedir Libro \\w+")) {
+                        String bookTitle = command.split(" ")[2];
 
-                            booksViews.printBooks(booksService.getBooksByAuthor(authorName));
-                            // ui.saveTrace(library, "getBooksByAuthor", authorName);
-                            break;
-                        default:
-                            break;
+                        booksViews.printBook(booksService.getBookByName(bookTitle));
+                        ui.saveTrace(library, "getBookByName", bookTitle);
+
+                    } else if (command.matches("Pedir Autor \\w+")) {
+                        String authorName = command.split(" ")[2];
+
+                        booksViews.printBooks(booksService.getBooksByAuthor(authorName));
+                        ui.saveTrace(library, "getBooksByAuthor", authorName);
+
+                    } else {
+                        ui.print("Comando no válido");
                     }
+
                     break;
 
                 case "B":
                     booksService = remoteService(Constants.URL_B);
 
-                    ui.print("1. Get Book by title");
-                    ui.print("2. Get Books by author");
-                    option = scanner.nextInt();
+                    ui.print("Available commands for library " + library);
+                    ui.print("1. Buscar Titulo [LIBRO]");
+                    ui.print("2. Buscar Autor [AUTOR]");
 
-                    switch (option) {
-                        case 1:
-                            ui.print("Book title?");
-                            // String bookTitle = scanner.nextLine();
-                            String bookTitle = "Dune";
+                    // command = scanner.nextInt();
+                    // scanner.nextLine();
 
-                            booksViews.printBook(booksService.getTitleByName(bookTitle));
-                            // ui.saveTrace(library, "getBookByName", bookTitle);
-                            break;
-                        case 2:
-                            ui.print("Author name?");
-                            String authorName = scanner.nextLine();
+                    // switch (command) {
+                    // case 1:
+                    // ui.print("Book title?");
+                    // String bookTitle = scanner.nextLine();
 
-                            booksViews.printBooks(booksService.getTitlesByAuthor(authorName));
-                            // ui.saveTrace(library, "getBooksByAuthor", authorName);
-                            break;
-                        default:
-                            break;
-                    }
+                    // booksViews.printBook(booksService.getTitleByName(bookTitle));
+                    // ui.saveTrace(library, "getTitleByName", bookTitle);
+                    // break;
+                    // case 2:
+                    // ui.print("Author name?");
+                    // String authorName = scanner.nextLine();
+
+                    // booksViews.printBooks(booksService.getTitlesByAuthor(authorName));
+                    // ui.saveTrace(library, "getTitlesByAuthor", authorName);
+                    // break;
+                    // default:
+                    // break;
+                    // }
+
+                    break;
+
+                case "C":
+                    booksService = remoteService(Constants.URL_C);
+
+                    ui.print("Available commands for library " + library);
+                    ui.print("1. Encontrar Vol [LIBRO]");
+                    ui.print("2. Encontrar Autor [AUTOR]");
+
+                    // command = scanner.nextInt();
+                    // scanner.nextLine();
+
+                    // switch (command) {
+                    // case 1:
+                    // ui.print("Book title?");
+                    // String bookTitle = scanner.nextLine();
+
+                    // booksViews.printBook(booksService.getTitleByName(bookTitle));
+                    // ui.saveTrace(library, "getTitleByName", bookTitle);
+                    // break;
+                    // case 2:
+                    // ui.print("Author name?");
+                    // String authorName = scanner.nextLine();
+
+                    // booksViews.printBooks(booksService.getTitlesByAuthor(authorName));
+                    // ui.saveTrace(library, "getTitlesByAuthor", authorName);
+                    // break;
+                    // default:
+                    // break;
+                    // }
+
                     break;
                 default:
 
